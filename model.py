2f90e4946ec3924e3458c6272bcee98d29afa9f8
Pawel Radzik <pawel.radzik321@gmail.com>
2022-12-04 17:51:36 +0100

    basic data types and solution format

---
+++ model.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+#dane do rozwiązania
+numberOfmonths=12
+numberOfYears=5
+numberOfFields=15
+typesOfWine=4
+operationMap={0:"nawóź",1:"siejtyp1",2:"siejtyp2",3:"siejtyp3",4:"siejtyp4"}
+solution1=np.ndarray(shape=(numberOfmonths*numberOfYears,numberOfFields), dtype=float)
+sellingSolution=np.ndarray(shape=(numberOfmonths*numberOfYears,typesOfWine), dtype=float)
+
+
+#warunki poczatkowe
+maksStorage=200
+# vineGrowCoeff = np.ndarray(shape=(numberOfmonths*numberOfYears,numberOfFields), dtype=float)
+fertilizerCoeff=1.2
+vinePrice = np.ndarray(shape=(numberOfmonths*numberOfYears,typesOfWine), dtype=float)
+
+storageCost=np.ndarray(shape=(maksStorage), dtype=float)
+wineOnSoilGrow=np.ndarray(shape=(numberOfFields,typesOfWine,2), dtype=float)#2 nawozone neinawozone
+gatheringCost=2#per winogrono
+transportCost=3#tak samo jak butla liczone
+#dojrzewarka ewentualnie
+
+def rozw(rozw,warunki):
+    growrate = np.ndarray(shape=(numberOfFields), dtype=float)
+    # 0-100 rośnei winogrono
+    # ponad to zbiory
+    #doimplementować jak wzime sie sieje to umiera
+
+
