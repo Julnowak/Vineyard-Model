9cdd944625abb060fce483384689167b969a6a76
Julnowak <91898403+Julnowak@users.noreply.github.com>
2022-12-06 16:07:57 +0100

    Alternative version for years only

---
+++ alternatywa.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from typing import Union, List, Dict
+import random
+
+
+# Generator rozwiązania początkowego dla lat
+def generate_solution(max_magazine_capacity: int, max_fields_capacity: Union[List, Dict],
+                      min_fields_capacity: Union[List, Dict], number_of_years: int) -> np.ndarray:
+
+    fields_num = len(max_fields_capacity)
+    solution = np.zeros((fields_num, number_of_years))
+    print(solution)
+    for y in range(solution.shape[1]):
+        flaga = True
+        while flaga:
+            for fnr in range(fields_num):
+                gen = random.randint(min_fields_capacity[fnr], max_fields_capacity[fnr])
+                solution[fnr][y] = gen
+
+            if np.sum(solution[:, y]) <= max_magazine_capacity:
+                flaga = False
+
+    return solution
+
+
+# Test generatora
+m = 600
+l = [800, 600, 800]  # Ograniczenia górne
+h = [100, 100, 100]  # Ograniczenia dolne
+yrs = 1
+
+A = generate_solution(m, l, h, yrs)
+print(A)
+
+
+field_nr = 3
+
+all_types = {1: 'Barbera', 2: 'Chardonnay', 3: 'Nebbiolo', 4: 'Arneis',
+         5: 'Dolcetto', 6: 'Cortese', 7: 'Grignolino', 8: 'Erbaluce'}
+
+
+
+ch_types = {1: 'Barbera', 2: 'Chardonnay', 3: 'Nebbiolo'}
+
+# Losowana jakość gleby
+np.set_printoptions(precision=2)
+soil_quality = np.random.uniform(low=0.7, high=0.95, size=(field_nr, len(ch_types)))
+print(soil_quality)
+
