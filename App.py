47a6826aebf32f626f491b04db91edda9c13a9dd
Julnowak <91898403+Julnowak@users.noreply.github.com>
2022-12-27 20:14:30 +0100

    Progress bar, main is now useless, everything in app

--- App.py
+++ App.py
@@ -1,7 +1,10 @@
-from main import *
 from PyQt6.QtWidgets import *
 from PyQt6 import uic,QtGui
 import pyqtgraph as pg
+from Command_files import *
+from Generators import *
+from Wykresy import *
+import time
 from canvas import *
 
 class UI(QMainWindow):
@@ -16,20 +19,51 @@ class UI(QMainWindow):
         self.epsilon = 0.1
         self.max_iter = 50
 
-        self.ch1 = self.findChild(QCheckBox,"checkBox")
-        self.ch1.toggled.connect(self.showDetails)
 
-        # Odczyt typów wina
-        ######
-        self.v1 = self.findChild(QCheckBox, 'Barbera')
-        self.v2 = self.findChild(QCheckBox, 'Chardonnay')
-        self.v3 = self.findChild(QCheckBox, 'Nebbiolo')
-        self.v4 = self.findChild(QCheckBox, 'Arneis')
-        self.v5 = self.findChild(QCheckBox, 'Dolcetto')
-        self.v6 = self.findChild(QCheckBox, 'Cortese')
-        self.v7 = self.findChild(QCheckBox, 'Grignolino')
-        self.v8 = self.findChild(QCheckBox, 'Erbaluce')
-        ##########
+        self.st = self.findChild(QStackedWidget, "stackedWidget")
+        self.st2 = self.findChild(QStackedWidget, "stackedWidget_2")
+
+        # Zaczynamy od Menu
+        self.st.setCurrentIndex(1)
+        self.st2.setCurrentIndex(1)
+
+        self.bset = self.findChild(QPushButton, "btn_set")
+        self.bset.clicked.connect(lambda: self.st.setCurrentIndex(0))
+
+        self.b2 = self.findChild(QPushButton, "btn_page_1")
+        self.b2.clicked.connect(lambda: self.st.setCurrentIndex(1))
+
+        self.b3 = self.findChild(QPushButton, "btn_page_2")
+        self.b3.clicked.connect(lambda: self.st.setCurrentIndex(2))
+
+        self.b4 = self.findChild(QPushButton, "btn_page_3")
+        self.b4.clicked.connect(lambda: self.st.setCurrentIndex(3))
+
+        self.b4 = self.findChild(QPushButton, "btn_page4")
+        self.b4.clicked.connect(lambda: self.st.setCurrentIndex(4))
+
+        self.b5 = self.findChild(QPushButton, "btn_info")
+        self.b5.clicked.connect(lambda: self.st.setCurrentIndex(5))
+
+
+        self.n = self.findChild(QPushButton, "next")
+        self.n.clicked.connect(lambda: self.st2.setCurrentIndex(0))
+
+        self.n1 = self.findChild(QPushButton, "next_2")
+        self.n1.clicked.connect(lambda: self.st2.setCurrentIndex(2))
+
+        self.n2 = self.findChild(QPushButton, "next_3")
+        self.n2.clicked.connect(lambda: self.st2.setCurrentIndex(3))
+
+        self.p = self.findChild(QPushButton, "prev")
+        self.p.clicked.connect(lambda: self.st2.setCurrentIndex(1))
+
+        self.p1 = self.findChild(QPushButton, "prev_2")
+        self.p1.clicked.connect(lambda: self.st2.setCurrentIndex(0))
+
+        self.p2 = self.findChild(QPushButton, "prev_3")
+        self.p2.clicked.connect(lambda: self.st2.setCurrentIndex(2))
+
 
         self.input = self.findChild(QDoubleSpinBox, "eps")
         self.input2 = self.findChild(QSpinBox, "iter")
@@ -51,30 +85,20 @@ class UI(QMainWindow):
         self.button2.clicked.connect(lambda: self.input2.setValue(50))
 
 
-        self.st = self.findChild(QStackedWidget, "stackedWidget")
-        self.st2 = self.findChild(QStackedWidget, "stackedWidget_2")
+        #Odczyt typów wina
+        #####
+        self.v1 = self.findChild(QCheckBox, 'Barbera')
+        self.v2 = self.findChild(QCheckBox, 'Chardonnay')
+        self.v3 = self.findChild(QCheckBox, 'Nebbiolo')
+        self.v4 = self.findChild(QCheckBox, 'Arneis')
+        self.v5 = self.findChild(QCheckBox, 'Dolcetto')
+        self.v6 = self.findChild(QCheckBox, 'Cortese')
+        self.v7 = self.findChild(QCheckBox, 'Grignolino')
+        self.v8 = self.findChild(QCheckBox, 'Erbaluce')
+        ##########
 
-        # Zaczynamy od Menu
-        self.st.setCurrentIndex(1)
-        self.st2.setCurrentIndex(0)
-
-        self.bset = self.findChild(QPushButton, "btn_set")
-        self.bset.clicked.connect(self.go_to_set)
-
-        self.b2 = self.findChild(QPushButton, "btn_page_1")
-        self.b2.clicked.connect(self.go_to_menu)
-
-        self.b3 = self.findChild(QPushButton, "btn_page_2")
-        self.b3.clicked.connect(self.go_to_1)
-
-        self.b4 = self.findChild(QPushButton, "btn_page_3")
-        self.b4.clicked.connect(self.go_to_2)
-
-        self.b4 = self.findChild(QPushButton, "btn_page4")
-        self.b4.clicked.connect(self.go_to_3)
-
-        self.b5 = self.findChild(QPushButton, "btn_info")
-        self.b5.clicked.connect(self.go_to_info)
+        self.ch1 = self.findChild(QCheckBox, "checkBox")
+        self.ch1.toggled.connect(self.showDetails)
 
         # wykres
         self.gv = self.findChild(QGraphicsView, 'graphWidget')
@@ -93,9 +117,16 @@ class UI(QMainWindow):
 
         self.gv.showGrid(x=True, y=True)
 
-        # Inny wykres
 
+        self.pb = self.findChild(QProgressBar,"pb")
+        self.pb.setTextVisible(False)
 
+        # DO tabeli w ust
+        self.tab = self.findChild(QTableWidget,"tableWidget")
+        self.nr_field = self.findChild(QSpinBox,"fieldnum")
+        self.rt = self.findChild(QPushButton,"refreshtab")
+
+        self.rt.clicked.connect(lambda: self.refrtab())
 
     def plot(self, x, y, plotname, color):
         pen = pg.mkPen(color=color, width=2)
@@ -127,24 +158,6 @@ class UI(QMainWindow):
 
         print(self.epsilon, self.max_iter)
 
-    # Wszystkie przyciski działają
-    def go_to_set(self):
-        self.st.setCurrentIndex(0)
-
-    def go_to_menu(self):
-        self.st.setCurrentIndex(1)
-
-    def go_to_1(self):
-        self.st.setCurrentIndex(2)
-
-    def go_to_2(self):
-        self.st.setCurrentIndex(3)
-
-    def go_to_3(self):
-        self.st.setCurrentIndex(4)
-
-    def go_to_info(self):
-        self.st.setCurrentIndex(5)
 
     def start_tabu(self):
         ch_types = {1: 'Barbera', 6: 'Cortese', 7: 'Grignolino'}
@@ -174,14 +187,137 @@ class UI(QMainWindow):
         magazine_capacity = 600
         store_needs = [100, 100, 100]
 
-        tabu_search(sol, planting_cost,
+        # Tabu search wbudowany
+        self.tabu_search(sol, planting_cost,
                     IsFertilized, soil_quality,
                     fertilizer_bonus, fertilizer_cost,
                     harvest_cost, bottling_cost,
                     plants_per_bottle, transport_cost,
                     vineprice, magazine_cost, magazine_capacity, store_needs, ch_types,
                     10, self.max_iter, self.epsilon)
+        self.pb.setValue(0)
+        self.pb.setTextVisible(False)
         self.c.destroyer()
+
+    def tabu_search(self, beg_sol, planting_cost,
+                    IsFertilized, soil_quality,
+                    fertilizer_bonus, fertilizer_cost,
+                    harvest_cost, bottling_cost,
+                    plants_per_bottle, transport_cost,
+                    vineprice, magazine_cost, magazine_capacity, store_needs, ch_types,
+                    tabu_length=10, max_iter=50, epsilon=0.1):
+
+        gain, loss = ocena(beg_sol, planting_cost,
+                           IsFertilized, soil_quality,
+                           fertilizer_bonus, fertilizer_cost,
+                           harvest_cost, bottling_cost,
+                           plants_per_bottle, transport_cost,
+                           vineprice, magazine_cost, magazine_capacity, store_needs)
+
+        sol_present_yourself(gain, loss, beg_sol, ch_types)
+
+        TL = []
+        avgMemory = np.zeros((2 * beg_sol.shape[0] * beg_sol.shape[1] * beg_sol.shape[
+            2]))  # pamiec srednioteminowa zlicza rozwiazania dane
+        streak = 0
+        streaknum = -1
+
+        solution = beg_sol
+        past_sol = sum(gain) - sum(loss)
+
+        # Najlepsze
+        bs_solution = solution.copy()
+        bs = sum(gain) - sum(loss)
+
+        gain_rem = 0
+        loss_rem = 0
+
+        stop_iter = False
+        stop_eps = False
+        counter = 0
+        self.pb.setTextVisible(True)
+        self.pb.setMaximum(max_iter)
+        limsta = []
+
+        while not (stop_iter or stop_eps):
+            self.pb.setValue(counter)
+            mapa = generateAllsolutions(solution)
+            neigh = [k for k, _ in mapa.items()]
+
+            n_rem = None
+            value = None
+            maxi = -np.inf
+            maxval = -np.inf
+
+            for n in neigh:
+
+                gain, loss = ocena(mapa[n], planting_cost,
+                                   IsFertilized, soil_quality,
+                                   fertilizer_bonus, fertilizer_cost,
+                                   harvest_cost, bottling_cost,
+                                   plants_per_bottle, transport_cost,
+                                   vineprice, magazine_cost, magazine_capacity, store_needs)
+
+                # + funkcja aspiracji
+                value = sum(gain) - sum(loss)
+                if n not in TL and value - avgMemory[n] * 2 > maxi:
+                    maxi = sum(gain) - sum(loss) - avgMemory[n] * 2  # no jak było wybierane to mniej
+                    maxval = sum(gain) - sum(loss)
+                    gain_rem = gain
+                    loss_rem = loss
+                    n_rem = n
+            # print(n_rem)
+
+            limsta.append(maxval)
+
+            avgMemory[n_rem] = avgMemory[n_rem] + 1
+            if n_rem == streaknum:
+                streak = streak + 1
+            else:
+                streaknum = n_rem
+                streak = 1
+            if streak > 99999:
+                # wybralismy 9999 to smao rozw zróbmy coś dzikiego
+                pass
+            limsta.append(value)
+
+            if maxval >= bs:
+                solution = mapa[n_rem].copy()
+            else:
+                if len(TL) < tabu_length:
+                    TL.append(generateAntiNum(n_rem))
+                else:
+                    TL.pop(0)
+                    TL.append(generateAntiNum(n_rem))
+                solution = mapa[n_rem].copy()
+
+            if counter > max_iter:
+                stop_iter = True
+
+            if abs(past_sol - maxval) <= epsilon:
+                stop_eps = True
+
+            past_sol = maxval
+
+            counter += 1
+            print(counter)
+            if counter >= max_iter:
+                stop_iter = True
+
+        self.pb.setValue(max_iter)
+        print(limsta)
+
+        plt.plot(limsta)
+        plt.title('Wykres wartości funkcji celu')
+        plt.show()
+
+        sol_present_yourself(gain_rem, loss_rem, bs_solution, ch_types)
+
+        return bs_solution
+
+    def refrtab(self):
+        print(int(self.nr_field.text()))
+        self.tab.setRowCount(int(self.nr_field.text()))
 
 
 app = QApplication([])
